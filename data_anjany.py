b6e27dbe5ef6bdc7b30691cbc0a08f17cff44e2e
jankukacka <jan.kukacka@tum.de>
2018-02-22 13:37:31 +0100

    Added: Test on Anjany's precomputed data.

---
+++ data_anjany.py
@@ -0,0 +1,375 @@
+# ------------------------------------------------------------------------------
+#  Single Shot Multibox Detector for Vertebra detection
+#  Jan Kukacka, 2/2018
+#  jan.kukacka@tum.de
+# ------------------------------------------------------------------------------
+#  Data generator from Anjany's files
+# ------------------------------------------------------------------------------
+
+# --
+import numpy as np
+import os
+import random
+# --
+from math import sqrt
+# --
+from data_augmenter import DataAugmenter
+from anchor_generator_layer import AnchorGenerator
+from anchor_assignment import Match
+
+import dataset_sdk as dsdk
+import dataset_sdk.bbox
+import dataset_sdk.imageset
+# --
+
+class AnjanyDataGenerator(object):
+    '''
+    Data generator class.
+    Loads a dataset and yields batches in Generate function.
+    '''
+
+    def __init__(self, batch_size, folder_path, padding=0, min_voxels=500,
+                 max_images=-1, use_two_classes=False, anchor_generator=None,
+                 overlap_threshold=.5, match_anchors=True, min_wh_ratio=0.3):
+        '''
+        Initializes the data generator.
+
+        # Arguments:
+            - batch_size: positive int. number of samples per batch.
+            - folder_path: string. path to the folder with the data.
+            - padding: positive int. Number of pixels to use as padding around
+                       GT bounding boxes. Default 0.
+            - min_voxels: positive int. Minimum number of pixels to consider a
+                          bounding box. Smaller ones are ignored. Default 500.
+            - max_images: int. Limits the number of images to use for the
+                          generator. If max_images < 0 or
+                          max_images > total_images, all images will be used.
+        '''
+        self.batch_size = batch_size
+        self.overlap_threshold = overlap_threshold
+        self.match_anchors = match_anchors
+        self.min_wh_ratio = min_wh_ratio
+
+        # 3 vertebra categories
+        if not use_two_classes:
+            self.classes = ('__background__', # always index 0
+                             'cervical', 'thoracic', 'lumbar')
+        else:
+            self.classes = ('__background__', 'spine')
+
+        # Load dataset
+        print 'Loading dataset', folder_path
+        self.images = {}
+        self.image_index = []
+        for filename in os.listdir(folder_path):
+            if filename.endswith('.npz'):
+                self.images[filename] =  np.load(os.path.join(folder_path, filename))['im'][::-1,:]
+                if 'sagittal' in folder_path:
+                    self.images[filename] =  np.transpose(self.images[filename], (1,0))
+                self.image_index.append(filename)
+        print 'Finished.'
+
+        ## ZMUV normalizaton
+        means = []
+        stdevs = []
+        sizes = []
+        for im in self.image_index:
+            means.append(np.mean(self.images[im]))
+            stdevs.append(np.var(self.images[im]))
+            sizes.append(np.size(self.images[im]))
+        means = np.array(means)
+        stdevs = np.array(stdevs)
+        sizes = np.array(sizes)
+        mean = np.sum(means * sizes) / np.sum(sizes)
+        var = np.sum((sizes - 1)*stdevs) / np.sum(sizes - 1)
+        std = np.sqrt(var)
+        for im in self.image_index:
+            self.images[im] = (self.images[im] - mean)/std
+        print 'ZMUV finished.'
+        # print 'Preparing bounding boxes...'
+        # limit = len(self.images.keys()) if max_images < 0 else max_images
+        # self.image_index = self.images.keys()[:min(len(self.images.keys()),limit)]
+        # bboxes = {}
+        # for image in self.image_index:
+        #     bboxes[image] = dsdk.bbox.numpy_to_bbox_info(bbox_metadata[image],
+        #                                                  bbox_slices[image])
+        #
+        # # prepare bounding boxes
+        # self.filtered_bboxes = {image: dsdk.bbox.filter_bbox_info(bboxes[image],
+        #            drop_empty=True,
+        #            padding=padding,
+        #            min_voxels=min_voxels) for image in self.image_index}
+
+        # remove images with bad shape (too narrow, too wide)
+        to_remove = []
+        for image in self.image_index:
+            # if self.filtered_bboxes[image]['slice_count'] == 0:
+            #     to_remove.append(image)
+            #     continue
+            s = self.images[image].shape
+            if 1.*s[0]/s[1] < 0.3 or \
+               1.*s[1]/s[0] < 0.3: # originally .15
+               to_remove.append(image)
+        for image in to_remove:
+            self.image_index.remove(image)
+
+        # if len(self.classes) == 2:
+        #     ## Convert classes to 2 class case
+        #     for image in self.image_index:
+        #         bbox_info = self.filtered_bboxes[image]
+        #         for sl in bbox_info['slices']:
+        #             for bbox in sl['bboxes']:
+        #                 bbox['class'] = 1
+
+        self.steps_per_epoch = len(self.image_index) // batch_size
+
+        if anchor_generator is None:
+            self.anchor_generator = AnchorGenerator(feature_stride=32,
+                                                    offset=0,
+                                                    aspect_ratios=[sqrt(0.5), 1],
+                                                    scale=2)
+        else:
+            self.anchor_generator = anchor_generator
+
+
+    def Generate(self, shuffle=True):
+        print 'Warning: This generator generates no ground truth boxes.'
+        num_keys = len(self.image_index)
+
+        def _prepare_batch(inputs, targets):
+            max_dims = np.array([512,512])
+            max_bboxes = 0
+
+            for i in xrange(self.batch_size):
+                max_dims = np.maximum(max_dims, inputs[i].shape)
+                # num_bboxes =  len(targets[i]['slices'][0]['bboxes'])
+                # max_bboxes = max(num_bboxes, max_bboxes)
+            max_dims = ((max_dims + 31) / 32) * 32 # round up to 32-divisible dimension
+
+            input_tensor = np.zeros((self.batch_size, max_dims[0], max_dims[1],1))
+            # zero padded input for some reason
+            target_tensor = np.zeros((self.batch_size, max_bboxes, 8+len(self.classes)))
+
+            for i in xrange(self.batch_size):
+                s = inputs[i].shape
+                pad = (max_dims - s) // 2 # pad around all edges
+                input_tensor[i] = inputs[i].min() # pad image with the lowest value (simulating air)
+                input_tensor[i, pad[0]:pad[0]+s[0], pad[1]:pad[1]+s[1], 0] = inputs[i]
+
+                # bboxes = targets[i]['slices'][0]['bboxes']
+                # for j in xrange(len(targets[i]['slices'][0]['bboxes'])):
+                #     target_tensor[i,j, :5] = dsdk.bbox.bbox_to_ccwhl(bboxes[j])
+
+            anchors = self.anchor_generator.Generate(input_tensor.shape)
+            ## Match anchors to the ground truth boxes
+            target_tensor = Match(target_tensor, anchors, len(self.classes),
+                                  self.overlap_threshold,
+                                  (input_tensor.shape[2],input_tensor.shape[1]))
+
+
+            return input_tensor, target_tensor
+
+        targets = []
+        inputs = []
+        while True:
+            if shuffle:
+                random.shuffle(self.image_index)
+            keys = self.image_index
+            for key in keys:
+                # targets.append(self.filtered_bboxes[key])
+                inputs.append(self.images[key])
+
+                if len(inputs) == self.batch_size:
+                    yield _prepare_batch(inputs, targets)
+                    inputs = []
+                    targets = []
+
+class OnlineDataGenerator(object):
+    '''
+    Data generator class.
+    Loads a dataset and yields batches in Generate function.
+    Unlike the DataGenerator, OnlineDataGenerator performs data augmentation
+    on-the-fly. Generates sagittal projections.
+    '''
+
+    def __init__(self, batch_size, imageset_name, cts_root_path, settings,
+                 padding=0, min_voxels=500,max_images=-1, use_two_classes=False,
+                 return_anchors=False, anchor_generator=None, overlap_threshold=.5,
+                 match_anchors=True, min_wh_ratio=0.3):
+        '''
+        Initializes the data generator.
+
+        # Arguments:
+            - batch_size: positive int. number of samples per batch.
+            - imageset_name: string. Name of the imageset to use.
+            - cts_root_path: string. Path to the root folder of the cts dataset.
+            - padding: positive int. Number of pixels to use as padding around
+                        GT bounding boxes. Default 0.
+            - min_voxels: positive int. Minimum number of pixels to consider a
+                        bounding box. Smaller ones are ignored. Default 500.
+            - max_images: int. Limits the number of images to use for the
+                        generator. If max_images < 0 or
+                        max_images > total_images, all images will be used.
+            - return_anchors: bool. If true, generator returns the anchors along
+                        with the offsets as the last four dimensions.
+                        Default False.
+            - use_two_classes: bool. If true, generator converts the labels of
+                        the vertebrae from [0..3] to [0,1]. Default False.
+            - anchor_generator: AnchorGenerator instance or a list thereof.
+                        If None (default), a generator with default settings
+                        is used. If supplying a list, they must be in the same
+                        order as predictions are concatenated in the used net.
+            - settings: kwargs for DataAugmenter. For details see DataAugmenter.
+            - overlap_threshold: float in range [0;1]. Minimum threshold to
+                        consider an anchor to be responsible for a GT box.
+            - match_anchors: bool. Default True. If True, generates data as numpy
+                        arrays of anchors and their desired offsets. If False,
+                        generates data in the same format as the DataGenerator,
+                        i.e. array of gt bboxes.
+            - min_wh_ratio: positive float. Smallest width/height or height/width
+                        ratio of generated image to be accepted. Default 0.3.
+        '''
+        self.batch_size = batch_size
+        self.padding = padding
+        self.min_voxels = min_voxels
+        self.return_anchors = return_anchors
+        self.overlap_threshold = overlap_threshold
+        self.match_anchors = match_anchors
+        self.min_wh_ratio = min_wh_ratio
+
+        # 3 vertebra categories
+        if not use_two_classes:
+            self.classes = ('__background__', # always index 0
+                             'cervical', 'thoracic', 'lumbar')
+        else:
+            self.classes = ('__background__', 'vetebra')
+
+        # Load dataset
+        self.imageset_list = dsdk.imageset.load_imageset_by_name(cts_root_path, imageset_name)
+        limit = len(self.imageset_list) if max_images < 0 else max_images
+        self.imageset_list = self.imageset_list[:min(len(self.imageset_list),limit)]
+
+        self.steps_per_epoch = len(self.imageset_list) // batch_size
+
+        self.augmenter = DataAugmenter(cts_root_path, **settings)
+        if anchor_generator is None:
+            self.anchor_generator = AnchorGenerator(feature_stride=32,
+                                                    offset=0,
+                                                    aspect_ratios=[sqrt(0.5), 1],
+                                                    scale=2)
+        else:
+            self.anchor_generator = anchor_generator
+
+
+    def get_augmented_img(self, image_id, depth=0):
+        '''
+        Returns an augmented version of an image from the ct-spine dataset.
+
+        # Arguments
+            - image_id: positive int. int-id of the desired image.
+            - depth: internal flag for stopping recursion in case a valid image
+                    generation fails.
+
+        # Returns
+            - img: numpy array of shape (height, width)
+            - bbox_info: bbox_info dictionary. For more info see dataset_sdk.bbox
+
+            If method fails to generate a valid image, it prints a message and
+            returns (None, None) tuple.
+        '''
+        if depth > 5:
+            print 'Could not generate valid input from image', image_id
+            return None, None
+
+        img, bbox_info = self.augmenter.get_image(image_id)
+        # post-process and validate
+        bbox_info = dsdk.bbox.filter_bbox_info(bbox_info,
+                   drop_empty=True,
+                   padding=self.padding,
+                   min_voxels=self.min_voxels)
+
+        ## Invalid images:
+        if bbox_info['slice_count'] == 0:
+            ## No bboxes
+            return OnlineDataGenerator.get_augmented_img(self,image_id, depth+1)
+        s = img.shape
+        if 1.*s[0]/s[1] < self.min_wh_ratio or \
+           1.*s[1]/s[0] < self.min_wh_ratio:
+           ## Bad w/h ratio
+           return OnlineDataGenerator.get_augmented_img(self,image_id, depth+1)
+
+        ## all good!
+        if len(self.classes) == 2:
+            ## Convert classes to 2 class case
+            for sl in bbox_info['slices']:
+                for bbox in sl['bboxes']:
+                    bbox['class'] = 1
+        return img, bbox_info
+
+    def Generate(self, shuffle=True):
+        num_keys = len(self.imageset_list)
+
+        def _prepare_batch(inputs, targets):
+            max_dims = np.array([32,32])
+            max_bboxes = 0
+
+            for i in xrange(self.batch_size):
+                max_dims = np.maximum(max_dims, inputs[i].shape)
+                num_bboxes =  len(targets[i]['slices'][0]['bboxes'])
+                max_bboxes = max(num_bboxes, max_bboxes)
+
+            max_dims = ((max_dims + 31) / 32) * 32 # round up to 32-divisible dimension
+
+            input_tensor = np.zeros((self.batch_size, max_dims[0], max_dims[1],1))
+            # zero padded input for some reason
+            target_tensor = np.zeros((self.batch_size, max_bboxes, 8+len(self.classes)))
+
+            for i in xrange(self.batch_size):
+                s = np.array(inputs[i].shape)
+                pad = (max_dims - s) // 2 # pad around all edges
+                input_tensor[i] = inputs[i].min() # pad image with the lowest value (simulating air)
+                input_tensor[i, pad[0]:pad[0]+s[0], pad[1]:pad[1]+s[1], 0] = inputs[i]
+
+                bboxes = targets[i]['slices'][0]['bboxes']
+                for j in xrange(len(bboxes)):
+                    target_tensor[i,j, :5] = dsdk.bbox.bbox_to_ccwhl(bboxes[j])
+                    ## Adjust labels for spine bounding boxes
+                    if 'is_spine' in bboxes[j] and bboxes[j]['is_spine']:
+                        target_tensor[i,j,4] = 1
+                    target_tensor[i,j, :2] += pad[::-1] # adjust for padding
+
+            ## Old data format, now mostly for debugging
+            if not self.match_anchors:
+                return input_tensor, target_tensor
+
+            ## Generate anchors for this batch
+            anchors = self.anchor_generator.Generate(input_tensor.shape)
+            ## Match anchors to the ground truth boxes
+            target_tensor = Match(target_tensor, anchors, len(self.classes),
+                                  self.overlap_threshold,
+                                  (input_tensor.shape[2],input_tensor.shape[1]))
+
+            ## This includes also anchors in the data
+            if self.return_anchors:
+                target_tensor = np.concatenate((target_tensor, anchors), axis=-1)
+
+            return input_tensor, target_tensor
+
+        targets = []
+        inputs = []
+        while True:
+            if shuffle:
+                random.shuffle(self.imageset_list)
+            for image_id in self.imageset_list:
+                img, bbox_info = self.get_augmented_img(image_id)
+                if img is None:
+                    print 'Skipping an image', image_id
+                    ## In case no valid image could be generated from this sample...
+                    continue
+                targets.append(bbox_info)
+                inputs.append(img)
+
+                if len(targets) == self.batch_size:
+                    yield _prepare_batch(inputs, targets)
+                    inputs = []
+                    targets = []
---
+++ test_visual_anjany.py
@@ -0,0 +1,150 @@
+# ------------------------------------------------------------------------------
+#  Single Shot Multibox Detector for Vertebra detection
+#  Jan Kukacka, 2/2018
+#  jan.kukacka@tum.de
+# ------------------------------------------------------------------------------
+#  Testing of a trained SSD model with visualization of results for spine
+#  bounding box prediction
+# ------------------------------------------------------------------------------
+
+
+# --
+import numpy as np
+import cPickle
+from math import sqrt
+import matplotlib.pyplot as plt
+plt.rcParams['image.cmap'] = 'gray'
+# --
+from net import Residual_SSD
+from data_anjany import AnjanyDataGenerator
+from anchor_generator_layer import AnchorGenerator
+from utils import nms, aggregate_bboxes_ccwh
+from multibox_loss import MultiboxLoss
+# --
+
+
+aggregation_plane = 'coronal'#'sagittal'
+
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+if aggregation_plane == 'coronal':
+    aspect_ratios = [sqrt(.2), sqrt(.4)]
+    scales = (5,6.5)
+else:
+    aspect_ratios = [sqrt(2.5), sqrt(3.5)]
+    scales = (5,7.5)
+
+ag = AnchorGenerator(feature_stride=32,
+                     offset=0,
+                     aspect_ratios=aspect_ratios,
+                     scale=scales)
+gen = AnjanyDataGenerator(batch_size=50,
+                          folder_path='/media/Data/Datasets/xVertSeg.v1/aux_data/2d_reformations/{}/train/'.format(aggregation_plane),
+                          overlap_threshold=.5,
+                          anchor_generator=ag)
+
+x_test, y_test = gen.Generate(shuffle=False).next()
+print 'Data ready.'
+model = Residual_SSD(num_classes=2, use_bn=True, num_anchors=len(aspect_ratios)*len(scales))
+
+with open('output/residual_ssd/cts_{}_max_train_spine/epoch_150.pkl'.format(aggregation_plane), 'rb') as f:
+    w = cPickle.load(f)
+model.set_weights(w)
+print 'Model ready.'
+
+pred = model.predict(x_test, batch_size=2)
+print np.sum(pred[:,:,5:]>.9, axis=(1,2))
+
+anchors = ag.Generate(x_test.shape)
+
+#%%
+
+def display_img_and_boxes(img, pred, gt):
+    fig, ax = plt.subplots(figsize=(12, 12))
+    ax.imshow(img, aspect='equal')
+
+    # plot GT boxes
+    for i in xrange(gt.shape[0]):
+        ax.add_patch(
+            plt.Rectangle((gt[i][0]-.5*gt[i][2], gt[i][1]-.5*gt[i][3]),
+                          gt[i][2], gt[i][3], fill=False, edgecolor='blue',
+                          linewidth=2))
+    for i in xrange(pred.shape[0]):
+        ax.add_patch(
+            plt.Rectangle((pred[i][0]-.5*pred[i][2], pred[i][1]-.5*pred[i][3]),
+                          pred[i][2], pred[i][3], fill=False, edgecolor='lightgreen',
+                          linewidth=1))
+
+    plt.show()
+
+def pred2bbox(anchor, pred):
+    #result = np.zeros_like(anchor)
+    centers = anchor[:,:,2:]*pred[:,:,:2] + anchor[:,:,:2]
+    sizes = anchor[:,:,2:]*np.exp(pred[:,:,2:])
+    return np.concatenate((centers, sizes), axis=-1)
+
+img_index = 41
+img = x_test[img_index,:,:,0]
+preds = pred2bbox(anchors,pred[:,:,:4])[img_index]
+gts = pred2bbox(anchors,y_test[:,:,:4])[img_index]
+top_k = np.argsort(-np.max(pred[img_index,:,5:], axis=-1))[:10] # k = 1
+pos = pred[img_index,:,5] > .9
+
+aggregated = aggregate_bboxes_ccwh(preds[pos,:4], 'mean')
+display_img_and_boxes(img, aggregated[np.newaxis,:], gts[y_test[img_index,:,5]==1])
+aggregated = aggregate_bboxes_ccwh(preds[top_k,:4], 'mean')
+display_img_and_boxes(img, aggregated[np.newaxis,:], gts[y_test[img_index,:,5]==1])
+#%%
+keep = nms(pred[:,:,[0,1,2,3,5]][img_index, pos], sigma=0.01, cutoff=0.5)
+keep = np.where(pos)[0][keep]
+plt.hist(pred[img_index,:,5])
+plt.show()
+# print pred[img_index, top_k]
+# print y_test[img_index,:,:4]
+display_img_and_boxes(img, preds[top_k], gts[y_test[img_index,:,5]==1])
+display_img_and_boxes(img, preds[keep], gts[y_test[img_index,:,5]==1])
+
+#%%
+plt.hist(x_test.flatten())
+plt.show()
+#%%
+# --
+import numpy as np
+from math import sqrt
+import matplotlib.pyplot as plt
+plt.rcParams['image.cmap'] = 'gray'
+# --
+from data import OnlineSpineDataGenerator
+from anchor_generator_layer import AnchorGenerator
+from utils import aggregate_bboxes_ccwh
+
+aug_settings_val = {
+    'use_crop': False,
+    'zmuv_mean': -103.361759224,
+    'zmuv_std': 363.301491674,
+    'aggregation_plane': 'coronal',
+    'aggregation_scale':0.01
+}
+aspect_ratios = [sqrt(2.5), sqrt(3.5)]
+scales = (5,7.5)
+ag = AnchorGenerator(feature_stride=32,
+                     offset=0,
+                     aspect_ratios=aspect_ratios,
+                     scale=scales)
+gen_val = OnlineSpineDataGenerator(batch_size=1, imageset_name='valid_large',
+                                cts_root_path='/media/Data/Datasets/ct-spine',
+                                settings=aug_settings_val,
+                                overlap_threshold=.01,
+                                anchor_generator=ag)
+
+x_test, y_test = gen_val.Generate(shuffle=False).next()
+anchors = ag.Generate(x_test.shape)
+
+gts = pred2bbox(anchors,y_test[:,:,:4])[0]
+
+display_img_and_boxes(x_test[0,:,:,0], np.zeros((1,4)), gts[y_test[0,:,5]==1])
+
+# plt.imshow(x_test[0,:,:,0])
+# plt.show()
